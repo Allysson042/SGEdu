85c099d3b50ead7209c52948836f8e9ac9ed1f08
lucasleonardobs <barss.lucas@gmail.com>
2019-09-19 23:43:06 -0300

    Início do projeto

---
+++ src/br/ufrpe/SGEdu/aluno/Responsavel.java
@@ -0,0 +1,7 @@
+package br.ufrpe.SGEdu.aluno;
+
+public class Responsavel {
+    private String nome, login, senha;
+    private int id;
+    private Aluno[] aluno;
+}
---
+++ src/br/ufrpe/SGEdu/gestao/Coordenador.java
@@ -0,0 +1,4 @@
+package br.ufrpe.SGEdu.gestao;
+
+public class Coordenador {
+}
