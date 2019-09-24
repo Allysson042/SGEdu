3c640e0be59ce24319ad59a19bd0bca5ab4dea40
laisy <laisyferreira2011@gmail.com>
2019-09-24 18:15:55 -0300

    Modificações em Professor e Turma

--- src/br/ufrpe/SGEdu/turma/Turma.java
+++ src/br/ufrpe/SGEdu/turma/Turma.java
@@ -1,16 +1,26 @@
 package br.ufrpe.SGEdu.turma;
 
 import br.ufrpe.SGEdu.aluno.*;
+import br.ufrpe.SGEdu.gestao.Professor;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Turma {
     private String nome;
-    //private Aluno alunos[];
     private ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+    private ArrayList<Professor> professores = new ArrayList<Professor>();
 
+    private String[][] horário = new String[5][5];
 
     public Turma(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
         this.nome = nome;
     }
 
@@ -18,8 +28,8 @@ public class Turma {
         alunos.add(aluno);
     }
 
-    public String getNome() {
-        return nome;
+    public void addProfessor(Professor professor) {
+        professores.add(professor);
     }
 
     public void printaAlunos() {
