d06d2bd1f32252d6cfd9edc20ebf0b658f06a431
laisy <laisyferreira2011@gmail.com>
2019-11-18 14:37:59 -0300

    Adição da GUI

---
+++ src/SGEdu/GUI/LoginAlunoController.java
@@ -0,0 +1,18 @@
+package SGEdu.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginAlunoController {
+
+    @FXML
+    private Button btVoltar;
+    
+    
+    @FXML
+    void botaoVoltar() {
+    	System.out.println("voltar menu");
+    	Main.changeScreen("Menu");
+    }
+
+}
